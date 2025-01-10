f2b19ad16ac193e3f5b52632461b13ce4b2edbfc
salko-ua <github@salko-ua.de>
2025-01-10 21:48:00 +0200

    add methods

--- monkeytype/__main__.py
+++ monkeytype/__main__.py
@@ -3,7 +3,7 @@ from monkeytype.users import Users
 from pprint import pprint
 
 
-monkey = Authorization()
+monkey = Authorization("Njc3ZGE4NGIzM2VlMjdhYTEyYjYxYjViLm1nRkxkQlUzVlRERXFFRURrSFpoalNpLUVMbFFUbVFz")
 users = Users(monkey)
 
-pprint(users.get_personal_bests("time").personal_beats["15"].acc)
+pprint(users.get_tags())
